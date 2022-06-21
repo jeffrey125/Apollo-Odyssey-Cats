18753620a5e90356f1bf29e92982ef5f3bac365e
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-22 00:05:24 +0800

    feat: Added type Module and track in typeDefs Schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+  "The description of a Track"
   type Track {
     id: ID!
     title: String!
@@ -8,6 +9,9 @@ const typeDefs = gql`
     thumbnail: String
     length: Int!
     modulesCount: Int
+    description: String
+    numberOfViews: Int
+    modules: [Module!]!
   }
 
   "Author of a complete Track or a Module"
@@ -17,8 +21,20 @@ const typeDefs = gql`
     photo: String
   }
 
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
+  }
+
   type Query {
+    "This will return the list of track"
     tracksForHome: [Track!]!
+    "This will return the specific track"
+    track(id: ID!): Track
   }
 `;
 
