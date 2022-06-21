b84e1f64f097a1cf88b60abe1a91a5d115dafae8
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-22 00:06:10 +0800

    feat: Updated to connect to new resolvers

---
+++ client/src/pages/track.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+import TrackDetail from '../components/track-detail';
+import { Layout, QueryResult } from '../components';
+
+const GET_TRACK = gql`
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
+
+const Track = ({ trackId }) => {
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Track;
