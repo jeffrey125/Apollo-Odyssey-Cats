0eb9c5648f3c74654f14cdd3f33695c2afefb018
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-23 22:22:58 +0800

    fix: conflicting branch

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-<<<<<<< HEAD
-  uri: 'https://apollo-odyssey-cats.herokuapp.com/',
-=======
   uri: 'https://server-catstronauts.herokuapp.com/', // change to YOUR own production server
->>>>>>> master
   cache: new InMemoryCache(),
   name: 'web',
   version: '1.0',
