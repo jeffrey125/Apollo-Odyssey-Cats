c1e790982410e6be8b887754add3e28fc2b906c6
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-23 20:29:46 +0800

    refactor: Index.js for production

--- server/src/index.js
+++ server/src/index.js
@@ -20,8 +20,10 @@ async function startApolloServer(typeDefs, resolvers) {
 
   const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
   console.log(`
-        🚀  Server is running
-        🔉  Listening on port ${port}
-        📭  Query at ${url}
-      `);
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `);
 }
+
+startApolloServer(typeDefs, resolvers);
